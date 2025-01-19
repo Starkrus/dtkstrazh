c12097bd0da7b5195d037a9f6f7c9e95a56f10fa
Михаил Ратфельдер <ratf@mail.ru>
2025-01-19 18:05:41 +0500

    creating photos for products

--- resources/views/partials/home/roducts.blade.php
+++ resources/views/partials/home/roducts.blade.php
@@ -8,21 +8,21 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
             <!-- Карточка товара 1 -->
             <a href="/products" class="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:-translate-y-2 block">
-                <img src="{{ asset('images/product1.jpg') }}" alt="Дульный тормоз-компенсатор для АК-12" class="w-full h-48 object-cover rounded-lg mb-4" loading="lazy">
+                <img src="{{ asset('build/images/ak12.png') }}" alt="Дульный тормоз-компенсатор для АК-12" class="w-full h-48 object-cover rounded-lg mb-4" loading="lazy">
                 <h3 class="text-xl font-semibold text-gray-800 mb-2">Дульный тормоз для АК-12</h3>
                 <p class="text-gray-600">Высококачественный дульный тормоз-компенсатор для АК-12. Снижает отдачу и повышает точность стрельбы.</p>
             </a>
 
             <!-- Карточка товара 2 -->
             <a href="/products" class="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:-translate-y-2 block">
-                <img src="{{ asset('images/product2.jpg') }}" alt="Дульный тормоз-компенсатор для СВД" class="w-full h-48 object-cover rounded-lg mb-4" loading="lazy">
+                <img src="{{ asset('build/images/svd.png') }}" alt="Дульный тормоз-компенсатор для СВД" class="w-full h-48 object-cover rounded-lg mb-4" loading="lazy">
                 <h3 class="text-xl font-semibold text-gray-800 mb-2">Дульный тормоз для СВД</h3>
                 <p class="text-gray-600">Прочный и надежный дульный тормоз для СВД. Изготовлен из нержавеющей стали.</p>
             </a>
 
             <!-- Карточка товара 3 -->
             <a href="/products" class="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:-translate-y-2 block">
-                <img src="{{ asset('images/product3.jpg') }}" alt="Дульный тормоз-компенсатор для АК-74" class="w-full h-48 object-cover rounded-lg mb-4" loading="lazy">
+                <img src="{{ asset('build/images/ak74.png') }}" alt="Дульный тормоз-компенсатор для АК-74" class="w-full h-48 object-cover rounded-lg mb-4" loading="lazy">
                 <h3 class="text-xl font-semibold text-gray-800 mb-2">Дульный тормоз для АК-74</h3>
                 <p class="text-gray-600">Идеальный выбор для АК-74. Улучшает стабильность и точность стрельбы.</p>
             </a>
